42c64ba9fd84b697d693268c530de73fa4fa63ff
tahasafdari <41860385+tahasafdari@users.noreply.github.com>
2022-11-23 17:00:40 +0200

    2.13 Data for countries completed. need to fix 2.14

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,7 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const CountryInfo = ({ country }) => {
+    const [weatherInfo, setWeatherInfo] = useState({})
+    const ACCESS_KEY = process.env.REACT_APP_API_KEY
+    console.log(ACCESS_KEY)
+
+    useEffect(() => {
+        const ACCESS_KEY = process.env.REACT_APP_API_KEY
+        axios
+            .get(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${country.capital.toLowerCase()}&units=m`)
+            .then(response => {
+                console.log(response.data);
+                const temperature = response.data.current.temperature;
+
+
+                const weatherObject = {
+                    temperature: temperature
+                }
+                setWeatherInfo(weatherObject)
+            })
+    })
+
+
+
     return(
         <div>
             <h2>{country.name}</h2>
+            <p>Capital: {country.capital}</p>
+            <p>Population: {country.population}</p>
+            <h3>Languages:</h3>
+            <ul>
+                {country.languages.map(language => (
+                    <li key={language.name}>{language.name}</li>
+                ))}
+            </ul>
+            <h3>Flag:</h3>
+            <img src={country.flag} width={"200"} />
+
+            <h3>Weather in {country.capital}</h3>
+            <p>Temperature:{weatherInfo.temperature} Celcius</p>
+
+
         </div>
     )
 
